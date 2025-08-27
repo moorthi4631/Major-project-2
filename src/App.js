3986ffbf0a67e82d396f964d73d6c24d6666ad36
moorthi4631 <nmoorthi222@gmail.com>
2025-08-27 09:52:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Login from './components/Auth/Login';
+import Signup from './components/Auth/Signup';
+import Profile from './Pages/Profile';
+import Home from './Pages/Home';
+import Chat from './components/Chat/Chat';
+import ChatList from './components/Chat/ChatList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Container>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/chats" element ={<ChatList/>} />
+          <Route path="/chat/:chatId" element ={<Chat/>} />
+        </Routes>
+      </Container>
+    </Router>
   );
 }
 
